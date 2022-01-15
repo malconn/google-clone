b851a5099b0ae912ca6d6741b456492229d5f277
maicaosgz <malconaugusto@outlook.com>
2022-01-15 18:07:31 -0300

    initial commit (need atention at Routes file)

---
+++ src/components/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {Switch, Redirect, Route } from 'react-router-dom'
+import  Results  from './Results'
+
+export const Routes = () => {
+    return (
+        <div className="p-4">
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to="/search"/>
+                </Route>
+                <Route exact path={["/seacrh","/news","/images","videos"]}>
+                    <Results />
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
