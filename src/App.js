e9bd5a7b06e671578ccae368f035357bbcf017c2
malconn <malconaugusto@outlook.com>
2022-01-17 10:33:13 -0300

    starting to fix errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-import { Routes } from "./components/Routes";
+import { Routers } from "./components/Routers";
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -10,7 +10,7 @@ const App = () => {
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
         <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
-        <Routes />
+        <Routers />
         <Footer />
       </div>
     </div>
