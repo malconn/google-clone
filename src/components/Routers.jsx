6c920270d82f86937f6dd4d732200d58d374db4b
maicaosgz <malconaugusto@outlook.com>
2022-01-20 19:57:04 -0300

    fix: refacting syntax at routes following doc

--- src/components/Routers.jsx
+++ src/components/Routers.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import {BrowserRouter, Route, Navigate, Routes } from 'react-router-dom'
-import Results from './Results'
+import { Route, Navigate, Routes } from "react-router-dom";
+import Results from "./Results";
 
 export const Routers = () => {
   return (
     <div className="p-4">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/">
-            <Navigate to="/search" />
-          </Route>
-          <Route exact path={['/search', '/news', '/images', '/videos']}>
-            <Results />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Route exact path="/">
+        <Navigate to="/search" />
+      </Route>
+      <Route exact path={["/search", "/news", "/images", "/videos"]}>
+        <Results />
+      </Route>
     </div>
-  )
-}
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import App from "./App";
+import { Routers } from "./components/Routers";
 import "./global.css";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/" element={<Routers />}></Route>
+    </Routes>
   </Router>,
   document.getElementById("root")
 );
