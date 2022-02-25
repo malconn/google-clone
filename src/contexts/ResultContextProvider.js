3752a74c9534b16fd939f9e4bbfb72fec576edc0
maicaosgz <malconaugusto@outlook.com>
2022-02-24 21:31:18 -0300

    feat: changing api key and see other possibilities before decide to remove project or just stop it

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,36 +1,39 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from "react";
 
-const ResultContext = createContext()
+const ResultContext = createContext();
 
-const baseUrl = 'https://google-search1.p.rapidapi.com/'
+const baseUrl = "https://google-search1.p.rapidapi.com/";
 
 export const ResultContextProvider = ({ children }) => {
-  const [results, setResults] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [searchTerm, setSearchTerm] = useState('')
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getResults = async (type) => {
-    setIsLoading(true)
+    setIsLoading(true);
 
     const response = await fetch(`${baseUrl}${type}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-rapidapi-host': 'google-search1.p.rapidapi.com',
-        'x-rapidapi-key': '25bca458e6msh34f2204bc2ef7d1p1a9be6jsnea80a9565511'
-      }
-    })
+        "x-rapidapi-host": "google-search1.p.rapidapi.com",
+        // "x-rapidapi-key": "d5b263d64emshb0b34fb157ad9b3p10857ejsn79a540514244",
+      },
+    });
 
     const data = await response.json();
-    const link = data?.organic?.sitelinks;
-    console.log(data,data?.organic[0]?.sitelinks[0]?.href);
+    // const link = data?.organic?.sitelinks;
+    // console.log(data, data?.organic[0]?.sitelinks[0]?.href);
+    console.log(data);
     setResults(data);
     setIsLoading(false);
-  }
-  return(
-    <ResultContext.Provider value={{ getResults,results,searchTerm,setSearchTerm,isLoading }}>
+  };
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
       {children}
     </ResultContext.Provider>
-  )
-}
+  );
+};
 
 export const useResultContext = () => useContext(ResultContext);
